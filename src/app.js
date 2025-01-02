9a6e2971e27e57b8a50cc3884ebc411b7cfb46ff
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-02 22:47:33 +0530

    Playing with routes order

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
+// app.use("/", (req, res) => {
+//   res.send("Hello World");
+// });
+app.use("/hello/2", (req, res) => {
+  res.send("Abra ka dabra!!!!");
+});
+
+app.use("/hello", (req, res) => {
   res.send("Hello World");
 });
 
