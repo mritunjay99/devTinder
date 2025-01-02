1454143b22f51a38be52d04b6cea44d74b4f869d
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-02 22:04:02 +0530

    Created express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/test", (req, res) => {
+  res.send("This is a test");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello");
+});
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
