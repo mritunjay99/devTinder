6773041a67c113bcc1242371ea124535e04aee02
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-04 22:59:09 +0530

    Added try catch block to catch the error and send the appropriate status code & message

--- src/app.js
+++ src/app.js
@@ -2,25 +2,14 @@ const express = require("express");
 const { userAuth, adminAuth } = require("./middleware");
 const app = express();
 
-app.use("/admin", adminAuth);
-
-// app.use("/user", userAuth);
-app.get("/admin/getAllData", (req, res) => {
-  res.status(200).send("Sent all data");
+app.get("/users", (req, res) => {
+  try {
+    throw new Error("Not valid!!");
+  } catch (err) {
+    res.status(401).send("Not valid!!");
+  }
 });
 
-app.delete("/admin/deleteData", (req, res) => {
-  res.status(200).send("Deleted data");
-});
-
-//alternatively auth middleware can be used like this
-app.get("/user/profile", userAuth, (req, res) => {
-  res.status(200).send("User Profile");
-});
-
-app.post("/user/save", userAuth, (req, res) => {
-  res.status(200).send("User Saved");
-});
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
