d910fda57abe557c26c75e25a26ba5bf6170302c
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-04 11:15:29 +0530

    Playing with multiple route handlers and how to call them

--- src/app.js
+++ src/app.js
@@ -2,18 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({
-    firstName: "John",
-    lastName: "Doe",
-  });
-});
-
-app.get("/users", (req, res) => {
-  console.log(req.query);
-  res.send(req.query);
-});
+//we can have multiple route handlers in one middleware
+app.use(
+  "/users",
+  (req, res, next) => {       //route handler
+    console.log("Response1!!");
+    // res.send("hello world!");  // if you send the response right now it won't be able to send the response in 2nd handler.
+    next(); // for calling the next route handler we have to use next() function.
+  },
+  (req, res) => {
+    console.log("Response2!!");
+    res.send("hello world2!");
+  }
+);
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
