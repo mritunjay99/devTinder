948805fd1d747ae5027030fe7aeb9982fdf8f1c0
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-06 08:45:00 +0530

    Added feed api to get all the user data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ const app = express();
 
 app.use(express.json()); //acts as a middleware for converting the incoming req' json object to js object
 
+//get user by email id
+app.get("/user", async (req, res) => {
+  const userFirstName = req.body.firstName;
+  try {
+    const user = await User.findOne({ firstName: userFirstName }); //if condition is not passed it will return any arbitary documents
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//feed api -get all the users
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   //saving data to mongodb
   const user = new User(req.body);
