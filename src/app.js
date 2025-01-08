39d03d9358fd8d0e9d593070546b1635d5d25c3c
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-08 23:06:41 +0530

    Setting values in cookie & reading from it

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const { connectDb } = require("./config/database");
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validator");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json()); //acts as a middleware for converting the incoming req' json object to js object
+app.use(cookieParser())
 
 //get user by email id
 app.get("/user", async (req, res) => {
@@ -42,6 +44,7 @@ app.post("/login", async (req, res) => {
     }
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (isValidPassword) {
+      res.cookie("token", "dgcbydbfmsdfchdckjdcbasjhdcvdnc");
       res.send("Login successful!!!");
     } else {
       throw new Error("Invalid credentials!!!");
@@ -49,6 +52,11 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
+});
+
+app.get("/profile", (req, res) => {
+  console.log(req.cookies);
+  res.send("Welcome to profile page");
 });
 
 app.post("/signup", async (req, res) => {
