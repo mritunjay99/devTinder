4b8803000181565bd1f8b39b7b9eebf8dd3a79cc
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-01-14 14:55:48 +0530

    Added api to get the feed for a particular user

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const ConnectionRequestModel = require("../models/connectionRequest");
 const { userAuth } = require("../middleware");
+const User = require("../models/user");
 
-const USER_SAFEDATA = "firstName lastName about skills gender";
+const USER_SAFEDATA = "firstName lastName about skills gender photoURL";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -37,13 +38,50 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
       .populate("toUserId", USER_SAFEDATA);
 
     const data = requests.map((row) => {
-      if (row.fromUserId._id.equals(loggedInUser._id)) {  //you can not directly compare mongoose id using equal operator
+      if (row.fromUserId._id.equals(loggedInUser._id)) {
+        //you can not directly compare mongoose id using equal operator
         return row.toUserId;
       }
       return row.fromUserId;
     });
     res.json({
       data,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+//api to get the feed for a particular user
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //we should only show those user with whom the connection request does not exist
+    //if the connection request is accepted/rejected we should not show it
+    //the user should not see himself in the feed api response
+
+    //getting all the requests which the user has sent or received
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    // .populate("fromUserId", "firstName")
+    // .populate("toUserId", "firstName");
+    // console.log(connectionRequests);
+
+    const hideUsers = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsers.add(req.fromUserId);
+      hideUsers.add(req.toUserId);
+    });
+    // console.log(hideUsers);
+
+    //getting all the users who doesn't exist in "hideUsers" set
+    const users = await User.find({
+      _id: { $nin: Array.from(hideUsers) },
+    }).select(USER_SAFEDATA);
+
+    res.json({
+      data: users,
     });
   } catch (err) {
     res.status(400).send("Error: " + err.message);
